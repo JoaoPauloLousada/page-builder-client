c50e086129966ea98cfa1641352819354609aec6
Joao Paulo Lousada <joao@imaginationmedia.com>
2020-06-13 14:03:12 -0300

    Bootstrap grid example

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import './app.scss';
+import React from "react";
+import "./app.scss";
+import Row from "../components/bootstrap/Row/Row";
+import Container from "../components/bootstrap/Container/Container";
+import Col from "../components/bootstrap/Col/Col";
 
 function App() {
+  const Component = Row;
   return (
     <div className="App">
-      App
+      <Container>
+        <Row>
+          <Col xs={8} sm={10} md={7} lg={4} xl={1}>
+            col left
+          </Col>
+          <Col>col right</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
