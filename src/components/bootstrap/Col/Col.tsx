5bb4c2c21b18d94e8c251cd8be459aa9b1d6c9c9
Joao Paulo Lousada <joao@imaginationmedia.com>
2020-06-13 14:10:52 -0300

    Update Bootstrap Col

--- src/components/bootstrap/Col/Col.tsx
+++ src/components/bootstrap/Col/Col.tsx
@@ -1,22 +1,5 @@
-import React, { ReactNode } from "react";
-
-interface Props {
-  children: ReactNode;
-  xs?: number;
-  sm?: number;
-  md?: number;
-  lg?: number;
-  xl?: number;
-}
-
-const classPrefix = ["", "sm-", "md-", "lg-", "xl-"];
-
-const getClasses = (...sizes: Array<number | undefined>): string => {
-  console.log(sizes);
-  return sizes.reduce((acc, curr, idx) => {
-    return `${acc}${curr ? ` col-${classPrefix[idx]}${curr}` : ""}`;
-  }, "");
-};
+import React from "react";
+import { Props, getClasses } from "./ColHelper";
 
 const Col: React.FC<Props> = ({ children, xs, sm, md, lg, xl }: Props) => {
   const classes = getClasses(xs, sm, md, lg, xl);
