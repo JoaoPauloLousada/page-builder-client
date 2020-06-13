23ff5ceb006146229e017d5b6cfe52420d4572bd
Joao Paulo Lousada <joao@imaginationmedia.com>
2020-06-13 14:08:51 -0300

    Update Bootstrap Container

--- src/components/bootstrap/Container/Container.tsx
+++ src/components/bootstrap/Container/Container.tsx
@@ -1,25 +1,15 @@
-import React, { ReactNode } from "react";
-
-interface Props {
-  children: ReactNode;
-  fluid?: boolean;
-  customClass?: string;
-}
-
-const getCustomClass = (customClass?: string): string =>
-  customClass ? ` ${customClass}` : "";
-
-const getFluid = (fluid?: boolean): string => (fluid ? "-fluid" : "");
-
-const getClasses = (customClass?: string, fluid?: boolean): string =>
-  `container${getFluid(fluid)}${getCustomClass(customClass)}`;
+import React from "react";
+import { getClasses, Props } from "./ContainerHelper";
 
 const Container: React.FC<Props> = ({
   children,
   fluid,
   customClass,
 }: Props) => {
-  const classes = getClasses(customClass, fluid);
+  const classes = React.useMemo(() => getClasses(customClass, fluid), [
+    customClass,
+    fluid,
+  ]);
 
   return <div className={classes}>{children}</div>;
 };
---
+++ src/components/bootstrap/Container/ContainerHelper.ts
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+export interface Props {
+  children: ReactNode;
+  fluid?: boolean;
+  customClass?: string;
+}
+
+export const getCustomClass = (customClass?: string): string =>
+  customClass ? ` ${customClass}` : "";
+
+export const getFluid = (fluid?: boolean): string => (fluid ? "-fluid" : "");
+
+export const getClasses = (customClass?: string, fluid?: boolean): string =>
+  `container${getFluid(fluid)}${getCustomClass(customClass)}`;
